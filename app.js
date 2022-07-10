3383a763926f399f1071435944641272a35f08f5
SAI MOHAN <45478292+MohanCloud-stack@users.noreply.github.com>
2022-07-10 17:44:20 +0530

    changed p

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 const port=3000;
 app.get("/",(req,res)=>{
-res.send("<h1>Demo App </h1>");
+res.send("<h1>Demo App </h1> <p> this is a new pipeline </p>");
 })
 app.get("/products",(req,res)=>{
     res.send([{
