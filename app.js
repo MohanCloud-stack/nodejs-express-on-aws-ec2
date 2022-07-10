361c1a0f7a068c315ce01b529a7f465cbffe6b74
SAI MOHAN <45478292+MohanCloud-stack@users.noreply.github.com>
2022-07-10 16:45:18 +0530

    build a simple api

---
+++ app.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const app=express();
+const port=3000;
+app.get("/",(req,res)=>{
+res.send("<h1>Demo App </h1>");
+})
+app.get("/products",(req,res)=>{
+    res.send([{
+        productId:"101",
+        price:100
+    },{
+        productId:"102",
+        price:150
+    }])
+})
+app.listen(port,()=>{
+    console.log(`Demo app running on Port:${port}`)
+})
---
+++ scripts/application_stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#stopping existing node servers
+echo "Stopping the existing node servers"
+pkill node
